00d2a58fd53d5da710145fa83496e53efb25cc5c
Warren Yao <warrenyca905@gmail.com>
2019-05-27 01:35:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -20,7 +20,7 @@ class School
   
   def sort
     sorted = {}
-    roster.map do |grade, students|
+    roster.each do |grade, students|
       sorted[grade] = roster[grade].sort
     end
     # return sorted.sort
