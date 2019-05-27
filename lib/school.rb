57a8c5465baf978d8351e4f9c3c6f4ebb4d92809
Warren Yao <warrenyca905@gmail.com>
2019-05-27 01:23:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+  
+  attr_reader :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    # if !roster.keys.include?(grade)
+      roster[grade] ||= []
+    # end
+    roster[grade] << name
+  end
+  
+  def grade(grd)
+    return roster[grd]
+  end
+  
+  def sort
+    roster.map |grade, students| do
+      
+  end
+end
